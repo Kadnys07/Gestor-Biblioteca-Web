88a25c8bb9c9c4ef5281765d4beddf81d3a275e1
Kadnys07 <kadnysm|@gmail.com>
2025-10-29 19:13:16 -0300

    Verificando conexão banco de dados

--- config/db.js
+++ config/db.js
@@ -1,24 +1,45 @@
-const { Pool } = require('pg');
-require('dotenv').config(); // Carrega o .env 
+    const { Pool } = require('pg');
+    require('dotenv').config(); // Carrega o .env (só funciona em desenvolvimento)
 
-let pool;
+    let pool;
 
-if (process.env.POSTGRES_URL) {
-    pool = new Pool({
-        connectionString: process.env.POSTGRES_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
-} else {
+    // A Vercel/Supabase injeta esta variável 'POSTGRES_URL' automaticamente
+    if (process.env.POSTGRES_URL) {
+        // --- Bloco de Produção (Vercel) ---
+        console.log("A conectar à base de dados de Produção (Vercel/Supabase)..."); // Log para depuração
+        pool = new Pool({
+            connectionString: process.env.POSTGRES_URL,
+            ssl: {
+                rejectUnauthorized: false 
+            }
+        });
+    } else {
+        
+        console.log("A conectar à base de dados de Desenvolvimento (Local)..."); // Log para depuração
+        pool = new Pool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT,
+        });
+    }
+
     
-    pool = new Pool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT,
+    pool.connect((err, client, release) => {
+      if (err) {
+        return console.error('Erro ao adquirir cliente do pool:', err.stack);
+      }
+      console.log('Conexão bem-sucedida ao banco de dados!');
+      client.query('SELECT NOW()', (err, result) => {
+        release(); // Libera o cliente de volta para o pool
+        if (err) {
+          return console.error('Erro ao executar query de teste:', err.stack);
+        }
+         console.log('Query de teste executada:', result.rows); 
+      });
     });
-}
 
-module.exports = pool;
+
+    module.exports = pool;
+    
